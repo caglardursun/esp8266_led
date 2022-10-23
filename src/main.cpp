c60f83b931c5f1daa2d33138dce79f32df35db42
dursuncaglar@gmail.com <dursuncaglar@gmail.com>
2022-10-24 01:09:04 +0300

    I2C screen and test codes are added

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,36 @@
 #include <Arduino.h>
 #include "ledlight.h"
+#include "screen.h"
 
-LedLight ledLight;
+IICScreen* screen;
 
 void setup() {
- ledLight.Setup();
- Serial.begin(115200);    
-}
+    
+  // text display tests
+ Serial.begin(9600);
+ screen = new IICScreen();
 
+}
+int loop_count =1;
 void loop() 
 {
-    Serial.println("Please Enter A Number\nThe Color Numbers Are Red: 1 , Green :2, Blue : 3\n");
-    while (Serial.available() == 0) {}     //wait for data available
-    String readedValue = Serial.readString();  
-    Serial.print("You have printed : ");
-    Serial.print(readedValue);
-    Serial.println("");
+    String msg = String("Test case : ") +loop_count + String("");
+    screen->Print(msg);
+    loop_count++;
+    delay(100);
+    // Serial.println("Please Enter A Number\nThe Color Numbers Are Red: 1 , Green :2, Blue : 3\n");
+    // while (Serial.available() == 0) {}     //wait for data available
+    // String readedValue = Serial.readString();  
+    // Serial.print("You have printed : ");
+    // Serial.print(readedValue);
+    // Serial.println("");
 
-    int value = readedValue.toInt();
-    if (value > 0 && value < 124)
-    {
-      ledLight.TurnOn((Colors) value) ;  
-    }else{
-      Serial.println("Please enter a valid number again\n");
-    }      
+    // int value = readedValue.toInt();
+    // if (value > 0 && value < 124)
+    // {
+    //   ledLight.TurnOn((Colors) value) ;  
+    // }else{
+    //   Serial.println("Please enter a valid number again\n");
+    // }      
 }
 
--- src/screen.cpp
+++ src/screen.cpp
@@ -1,15 +1,42 @@
 #include "screen.h"
 
-LCDScreen::LCDScreen(/* args */)
+IICScreen::IICScreen(/* args */)
 {
+    Serial.begin(9600); 
+    delay(250); // wait for the OLED to power up
+    display.begin(i2c_Address, true); // Address 0x3C default
+    //display.setContrast (0); // dim display    
+    //display.display();
+    delay(250);
 
+   display.clearDisplay();
+  
+        // Draw bitmap on the screen
+    display.drawBitmap(0, 0, epd_bitmap_ada, 128, 64, 1);
+    display.display();
+    delay(1000);
 }
 
-LCDScreen::~LCDScreen()
+IICScreen::~IICScreen()
 {
 }
 
-void LCDScreen::Print(){
+void IICScreen::Print(String msg)
+{
+   display.clearDisplay();
+   delay(250);
+   display.setTextSize(1);   
+   display.setCursor(0, 0);   
+   display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
+   display.setTextSize(1);
+   display.print(msg);
+   display.display();
+   delay(250);   
+   display.clearDisplay();
+   delay(250);   
+}
 
+void IICScreen::Print(int number)
+{
 
 }
